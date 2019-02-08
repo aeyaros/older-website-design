8f977e067c99cb3853c6ff73847574226126692f
Andrew Yaros <aey24@drexel.edu>
2019-02-08 15:56:52 -0500

    Updates
    Rearranged some code in the index page
    
    Moved date-changing into a function
    
    index.js called at the beginning of the page; function calls are used in the body, rather than calling the entire script at the end of the page, which was stupid
    
    Improved preloading so it starts at whatever image is loaded first, rather than the image at index 0
    
    Increased slideshow animation transition time

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/****************************************
-javascript for homepage
-By Andrew Yaros
-For changing themes and the iframe source 
-Please insert after body tag
-****************************************/
+/************************
+ * JavaScript for index *
+ * page by Andrew Yaros *
+ ************************/
 
 //IMPORTANT keep track of the current theme
 var isDark = false;
@@ -14,7 +12,7 @@ var moonIcon = '<svg id="themetoggletag" width="16" height="16" viewBox="0 0 16 
 var sunIcon = '<svg id="themetoggletag" width="16" height="16" viewBox="0 0 16 16"><path id="themetoggleicon" d="M.669,7.792H3.646v.4H.669Zm2,5.25,2.1-2.112.288.288-2.1,2.1ZM2.958,2.67l2.113,2.1-.288.288-2.1-2.1ZM11.329,8A3.329,3.329,0,1,1,8,4.671,3.327,3.327,0,0,1,11.329,8ZM7.792,15.331V12.354h.4v2.977ZM8.208.669V3.646h-.4V.669ZM13.043,13.33l-2.114-2.1.289-.288,2.1,2.1Zm.288-10.372-2.1,2.113-.288-.288,2.1-2.1Zm2,5.25H12.354v-.4h2.977Z"/></svg>';
 
 /*	Original unicode icons for buttons: 
-	&#9728; sun		&#9790; moon 	&#9993; mail	*/
+	&#9728; sun		&#9790; moon 	&#9993; mail	 */
 
 //change the theme to a different css file
 //must be in the styles folder
@@ -61,8 +59,17 @@ function resetDisplay() {
 	////document.getElementById("iframeDisplay").contentDocument.body.style.filter = "none";
 }
 
-//This should be done after the body tag
-resetDisplay();
+//update the current copyright year
+function updateDate() {
+	//Change year to current year
+	document.getElementById("currentDate").innerHTML = (new Date).getFullYear();
+}
+
 /* If it is dark out and dark theme isn't loaded, then load the dark theme ;) */
 var theHour = parseInt((new Date).getHours());
 if((theHour < 7 || theHour >= 19) && !isDark) toggleTheme();
+
+
+
+//resetDisplay should be called after the body tag
+//updateDate should be called right after the currentDate div
