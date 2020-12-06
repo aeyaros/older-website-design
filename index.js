657ee8b09a18c38cfc94553b4d0c409420b31ac8
Andrew Yaros <aeyaros@gmail.com>
2020-12-06 03:14:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const innerDisplayOpaque = "1"; //can be .5 or 1 or whatev
 const innerDisplayTrans = "0"; //MUST BE ZERO!
 //const noblur = "none";
 //const blurred = "blur(20px)";
-var i;
 
 //variables for mouseover background tiles
 //there are two arrays for tiles - one for light and one for dark
@@ -42,9 +41,9 @@ var isDark = false;
 var userChangedTheme = false;
 
 //sun and moon button SVG icons
-var moonIcon = '<svg id="themetoggletag" width="16" height="16" viewBox="0 0 16 16"><path id="themetoggleicon" d="M13.84,9.75a6,6,0,0,1-5.8,4.039A5.8,5.8,0,0,1,2.16,7.977,5.961,5.961,0,0,1,6.246,2.211a5.829,5.829,0,0,0-.57,2.726,5.19,5.19,0,0,0,5.414,5.384A6.052,6.052,0,0,0,13.84,9.75Z"/></svg>';
+const moonIcon = '<svg id="themetoggletag" width="16" height="16" viewBox="0 0 16 16"><path id="themetoggleicon" d="M13.84,9.75a6,6,0,0,1-5.8,4.039A5.8,5.8,0,0,1,2.16,7.977,5.961,5.961,0,0,1,6.246,2.211a5.829,5.829,0,0,0-.57,2.726,5.19,5.19,0,0,0,5.414,5.384A6.052,6.052,0,0,0,13.84,9.75Z"/></svg>';
 
-var sunIcon = '<svg id="themetoggletag" width="16" height="16" viewBox="0 0 16 16"><path id="themetoggleicon" d="M.669,7.792H3.646v.4H.669Zm2,5.25,2.1-2.112.288.288-2.1,2.1ZM2.958,2.67l2.113,2.1-.288.288-2.1-2.1ZM11.329,8A3.329,3.329,0,1,1,8,4.671,3.327,3.327,0,0,1,11.329,8ZM7.792,15.331V12.354h.4v2.977ZM8.208.669V3.646h-.4V.669ZM13.043,13.33l-2.114-2.1.289-.288,2.1,2.1Zm.288-10.372-2.1,2.113-.288-.288,2.1-2.1Zm2,5.25H12.354v-.4h2.977Z"/></svg>';
+const sunIcon = '<svg id="themetoggletag" width="16" height="16" viewBox="0 0 16 16"><path id="themetoggleicon" d="M.669,7.792H3.646v.4H.669Zm2,5.25,2.1-2.112.288.288-2.1,2.1ZM2.958,2.67l2.113,2.1-.288.288-2.1-2.1ZM11.329,8A3.329,3.329,0,1,1,8,4.671,3.327,3.327,0,0,1,11.329,8ZM7.792,15.331V12.354h.4v2.977ZM8.208.669V3.646h-.4V.669ZM13.043,13.33l-2.114-2.1.289-.288,2.1,2.1Zm.288-10.372-2.1,2.113-.288-.288,2.1-2.1Zm2,5.25H12.354v-.4h2.977Z"/></svg>';
 
 /*	Original unicode icons for buttons: 
 	&#9728; sun		&#9790; moon 	&#9993; mail	 */
@@ -123,7 +122,7 @@ function setTranslucentSlide() {
 //will be appended to the head tag as an additional link tag
 function appendCSS(cssname) {
 	//create a new link element, set it's attributes
-	var newTheme = document.createElement("link");
+	let newTheme = document.createElement("link");
 	newTheme.setAttribute("type", "text/css");
 	newTheme.setAttribute("rel", "stylesheet");
 	newTheme.setAttribute("href", "./media/styles/" + cssname + ".css");
@@ -179,14 +178,14 @@ function toggleThemeAndRefresh() {
 
 //change theme based on time
 function setTheme() {
-	var theHour = parseInt((new Date).getHours());
+	let theHour = (new Date).getHours();
 	if(theHour < 7 || theHour >= 19) makeDark();
 	else makeLight();
 }
 
 //update the current copyright year
 function updateDate() { //Change year to current year
-	document.getElementById("currentDate").innerHTML = (new Date).getFullYear();
+	document.getElementById("currentDate").innerHTML = String((new Date).getFullYear());
 }
 
 //change the inner shadow div to an image
@@ -269,11 +268,11 @@ window.onload = function initial() {
 	resetDisplay();	
 
 	//preload images
-	var images = new Array();
+	let images = [];
 	/* Start preloading at the starting slide
 	Preload n images */
-	var currentSlideToPreload = startingSlide;
-	for (i = 0; i < n; i++) {
+	let currentSlideToPreload = startingSlide;
+	for (let i = 0; i < n; i++) {
 		images[currentSlideToPreload] = new Image();
 		images[currentSlideToPreload].src = slideNames[currentSlideToPreload%n];
 		currentSlideToPreload++; //increment index of slide; modulus n because we are looping over to the start
@@ -283,22 +282,22 @@ window.onload = function initial() {
 	setBackground(slideNames[startingSlide]);
 	
 	//preload images for mouseovers
-	var moreImages1 = new Array(); //light tiles
-	var moreImages2 = new Array(); //dark tiles
+	const moreImages1 = []; //light tiles
+	const moreImages2 = []; //dark tiles
 	
 	if (isDark) { //if dark, first load dark tiles, then light
-		for (i = 0; i < tileNames2.length; i++) {
+		for (let i = 0; i < tileNames2.length; i++) {
 			moreImages2[i] = new Image();
 			moreImages2[i].src = tileNames2[i];
-		} for (i = 0; i < tileNames1.length; i++) {
+		} for (let i = 0; i < tileNames1.length; i++) {
 			moreImages1[i] = new Image();
 			moreImages1[i].src = tileNames1[i];
 		}
 	} else { //otherwise load lighter tiles first
-		for (i = 0; i < tileNames1.length; i++) {
+		for (let i = 0; i < tileNames1.length; i++) {
 			moreImages1[i] = new Image();
 			moreImages1[i].src = tileNames1[i];
-		} for (i = 0; i < tileNames2.length; i++) {
+		} for (let i = 0; i < tileNames2.length; i++) {
 			moreImages2[i] = new Image();
 			moreImages2[i].src = tileNames2[i];
 		}
